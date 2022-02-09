9e9bc2d471974f68265282179aa6ed1df49ef8c1
Esteban Ricardo Mirizio <35462288+ermirizio@users.noreply.github.com>
2022-02-09 20:12:37 -0300

    a

--- test.sh
+++ test.sh
@@ -3,3 +3,9 @@ set -exo pipefail
 
 LOCALIP=${1:-'localhost'}
 RANDOM_USER=$(echo $RANDOM | md5sum | head -c 10 | tr -dc a-z0-9)
+
+ORGDATA="{"Status":"OK","Message":"Org created","Meta":"6204181723e7f200019a462e"}"
+
+ORGID=$(echo $ORGDATA | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["Meta"])')
+
+echo $ORGID
